22a946681cf109a8cd3f23f5fc5605d31d0e3c64
LucasBondeDeSouza <bondelucas77@gmail.com>
2024-05-03 16:55:34 -0300

    update Skills

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,44 +1,56 @@
-import React from "react";
+import { React, useRef } from "react";
 import Typical from 'react-typical';
 
 import './Header.css'
 
+import Avatar from '../assets/avatar.png'
+
 export default () => {
+    const skillsRef = useRef(null);
+
+    const scrollToSkills = () => {
+        skillsRef.current.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest', duration: 1000 }); // Ajuste a duração da rolagem aqui (1000ms = 1 segundo)
+    };
+
     return (
         <div className="Header--container">
             <div className="Header--nav">
-                <p>Skills</p>
+                <p onClick={scrollToSkills}>Skills</p>
                 <p>Projects</p>
-                <p>About</p>
                 <p>Contact</p>
             </div>
 
-            <div className="Header--avatar"></div>
+            <div className="Header--person">
+                <div className="Header--avatar">
+                    <img src={Avatar} alt="Avatar" />
+                </div>
 
-            <div className="Header--typical">
-                <p>
-                    I'm Lucas Bonde{' '}
-                    <Typical
-                        loop={Infinity}
-                        steps={[
-                            'Dev Web',
-                            3000,
-                            '',
-                            500,
-                            'Dev Front-End',
-                            3000,
-                            '',
-                            500,
-                            'Dev Back-End',
-                            3000,
-                            '',
-                            500,
-                            'UI/UX',
-                            3000
-                        ]}
-                    />
-                </p>
+                <div className="Header--typical">
+                    <p>
+                        I'm Lucas Bonde{' '}
+                        <Typical
+                            loop={Infinity}
+                            steps={[
+                                'Dev Web',
+                                3000,
+                                '',
+                                500,
+                                'Dev Front-End',
+                                3000,
+                                '',
+                                500,
+                                'Dev Back-End',
+                                3000,
+                                '',
+                                500,
+                                'UI/UX',
+                                3000
+                            ]}
+                        />
+                    </p>
+                </div>
             </div>
+            <div ref={skillsRef} />
         </div>
     )
 }
--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,11 +1,96 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import './Skills.css'
 
+import Logo_HTML from '../assets/logo-html.png'
+import Logo_CSS from '../assets/logo-css.png'
+import Logo_JS from '../assets/logo-js.png'
+import Logo_Python from '../assets/logo-python.png'
+import Logo_Java from '../assets/logo-java.png'
+import Logo_React from '../assets/logo-react.png'
+import Logo_Vue from '../assets/logo-vue.png'
+import Logo_MySql from '../assets/logo-mysql.png'
+import Logo_Git from '../assets/logo-git.png'
+
 export default () => {
+
     return (
-        <div className="Skills">
+        <div className="Skills--container">
             <p>Skills</p>
+
+            <div className="Skills--line">
+                <div className="Skills--loading_line">
+                    <div className="Skills--image">
+                        <img src={Logo_HTML} alt="Logo HTML" />
+                    </div>
+
+                    <div className="Skills--loading_bar html"></div>
+                </div>
+
+                <div className="Skills--loading_line">
+                    <div className="Skills--image">
+                        <img src={Logo_CSS} alt="Logo CSS" />
+                    </div>
+
+                    <div className="Skills--loading_bar css"></div>
+                </div>
+
+                <div className="Skills--loading_line">
+                    <div className="Skills--image">
+                        <img src={Logo_JS} alt="Logo js" />
+                    </div>
+
+                    <div className="Skills--loading_bar js"></div>
+                </div>
+
+                <div className="Skills--loading_line">
+                    <div className="Skills--image">
+                        <img src={Logo_Python} alt="Logo Python" />
+                    </div>
+
+                    <div className="Skills--loading_bar python"></div>
+                </div>
+
+                <div className="Skills--loading_line">
+                    <div className="Skills--image">
+                        <img src={Logo_Java} alt="Logo Java" />
+                    </div>
+
+                    <div className="Skills--loading_bar java"></div>
+                </div>
+
+                <div className="Skills--loading_line">
+                    <div className="Skills--image">
+                        <img src={Logo_React} alt="Logo React" />
+                    </div>
+
+                    <div className="Skills--loading_bar react"></div>
+                </div>
+
+                <div className="Skills--loading_line">
+                    <div className="Skills--image">
+                        <img src={Logo_Vue} alt="Logo Vue" />
+                    </div>
+
+                    <div className="Skills--loading_bar vue"></div>
+                </div>
+
+                <div className="Skills--loading_line">
+                    <div className="Skills--image">
+                        <img src={Logo_MySql} alt="Logo MySql" />
+                    </div>
+
+                    <div className="Skills--loading_bar mysql"></div>
+                </div>
+
+                <div className="Skills--loading_line">
+                    <div className="Skills--image">
+                        <img src={Logo_Git} alt="Logo Git" />
+                    </div>
+
+                    <div className="Skills--loading_bar git"></div>
+                </div>
+            </div>
         </div>
     )
 }
