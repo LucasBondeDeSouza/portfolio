8707491264a3fc2f9d40217787db66c050932ac0
LucasBondeDeSouza <bondelucas77@gmail.com>
2024-05-08 14:54:28 -0300

    atualização

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,6 +3,8 @@ import React from 'react'
 import './About.css'
 
 import Img_about from '../assets/img-about.png'
+import { FaArrowDown } from 'react-icons/fa';
+import Curriculo from '../assets/Curriculo.docx'
 
 export default () => {
     return (
@@ -14,9 +16,19 @@ export default () => {
                     <img src={Img_about} alt='Imagem about' />
                 </div>
 
-                <p>
-                    Olá! Sou Lucas Bonde, um entusiasta da tecnologia e estudante de Análise e Desenvolvimento de Sistemas apaixonado por resolver problemas por meio da programação. Busco constantemente expandir meu conhecimento e aprimorar minhas habilidades na criação de soluções inovadoras e eficientes. Com um forte interesse em Desenvolvimento Web, estou determinado a contribuir de forma significativa para a indústria da tecnologia. Além de meu entusiasmo pela programação, também valorizo a colaboração em equipe e estou sempre pronto para enfrentar novos desafios. Estou ansioso para compartilhar meu trabalho e experiências neste portfólio. Seja bem-vindo!
-                </p>
+                <div className='About--text'>
+                    <p>
+                        Olá! Sou Lucas Bonde, um entusiasta da tecnologia e estudante de Análise e Desenvolvimento de Sistemas apaixonado por resolver problemas por meio da programação. Busco constantemente expandir meu conhecimento e aprimorar minhas habilidades na criação de soluções inovadoras e eficientes. Com um forte interesse em Desenvolvimento Web, estou determinado a contribuir de forma significativa para a indústria da tecnologia. Além de meu entusiasmo pela programação, também valorizo a colaboração em equipe e estou sempre pronto para enfrentar novos desafios. Estou ansioso para compartilhar meu trabalho e experiências neste portfólio. Seja bem-vindo!
+                    </p>
+
+                    <div className="About--resume">
+                        <div className="About--resume--text">
+                            <h2>Baixe meu Portfólio</h2>
+                            <h2><FaArrowDown /></h2>
+                        </div>
+                        <a href={Curriculo} download='Curriculo_Lucas.docx'>Download do portfólio</a>
+                    </div>
+                </div>
             </div>
         </div>
     )
