a2c81e277671d89367d5f14b6c04d0b752b022b8
LucasBondeDeSouza <bondelucas77@gmail.com>
2024-05-08 13:39:09 -0300

    atualização

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,11 +2,17 @@ import React from 'react'
 
 import './About.css'
 
+import Img_about from '../assets/img-about.png'
+
 export default () => {
     return (
         <div id="about" className="About">
             <p>About Me</p>
-            <div className="About--content">
+            <div className="About--content toRight">
+                <div className='About--img'>
+                    <img src={Img_about} alt='Imagem about' />
+                </div>
+
                 <p>
                     Olá! Sou Lucas Bonde, um entusiasta da tecnologia e estudante de Análise e Desenvolvimento de Sistemas apaixonado por resolver problemas por meio da programação. Busco constantemente expandir meu conhecimento e aprimorar minhas habilidades na criação de soluções inovadoras e eficientes. Com um forte interesse em Desenvolvimento Web, estou determinado a contribuir de forma significativa para a indústria da tecnologia. Além de meu entusiasmo pela programação, também valorizo a colaboração em equipe e estou sempre pronto para enfrentar novos desafios. Estou ansioso para compartilhar meu trabalho e experiências neste portfólio. Seja bem-vindo!
                 </p>
