ee13dc41f0974cc6534aafc31b7c85bd8735a9bb
LucasBondeDeSouza <bondelucas77@gmail.com>
2024-05-09 15:41:57 -0300

    atualização

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,6 +1,8 @@
 import React from "react";
 import './Nav.css'
 
+import { FaArrowUp } from 'react-icons/fa';
+
 const Nav = () => {
     const scrollToSection = (id) => {
         const element = document.getElementById(id);
@@ -10,15 +12,21 @@ const Nav = () => {
     }
 
     return (
-        <header>
-            <h1 onClick={() => scrollToSection('main')}>Portfólio</h1>
-            <div className="Nav">
-                <p onClick={() => scrollToSection('about')}>About</p>
-                <p onClick={() => scrollToSection('skills')}>Skills</p>
-                <p onClick={() => scrollToSection('projects')}>Projects</p>
-                <p onClick={() => scrollToSection('contact')}>Contact</p>
+        <React.Fragment>
+            <header>
+                <h1 onClick={() => scrollToSection('main')}>Portfólio</h1>
+                <div className="Nav">
+                    <p onClick={() => scrollToSection('about')}>About</p>
+                    <p onClick={() => scrollToSection('skills')}>Skills</p>
+                    <p onClick={() => scrollToSection('projects')}>Projects</p>
+                    <p onClick={() => scrollToSection('contact')}>Contact</p>
+                </div>
+            </header>
+
+            <div onClick={() => scrollToSection('main')} className="Scroll-top">
+                <FaArrowUp />
             </div>
-        </header>
+        </React.Fragment>
     );
 }
 
